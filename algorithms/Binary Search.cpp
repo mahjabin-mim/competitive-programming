b2dfc2671e2b11fad648cc0f26b69396487906b9
Mahjabin Mim <mahjabinmim711@gmail.com>
2024-08-25 15:42:58 +0600

    Update Binary Search.cpp

--- algorithms/Binary Search.cpp
+++ algorithms/Binary Search.cpp
@@ -1,36 +1,49 @@
+/*
+Author: Mahjabin7
+Date: 25/08/24
+*/
 #include <bits/stdc++.h>
-#include <iostream>
-#include <set>
+#include<ext/pb_ds/assoc_container.hpp> 
+#define optimized ios::sync_with_stdio(false); cin.tie(0);
 
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-int main()
-{   
-    int n=10;
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() 
+{    
+    optimized
+    int n=10+1;
     int arr[n]={11,44,33,7,3,6,10,8,9,2};
     
     sort(arr,arr+n);
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
       cout<<arr[i]<<" ";
     }cout<<endl;
     
-    int x=10;
-    int l=0;
-    int r=n-1;
-    for(int i=0; i<n; i++){
-      while(l<=r){
+    int x=7;
+    int l=1;
+    int r=n;
+    int ans;
+    while(l<=r){
         int mid=(l+r)/2;
         if(arr[mid]==x){
-          r=mid-1;
-        }else if(arr[mid]>x){
+          ans=mid;
+          break;
+        }
+        if(arr[mid]>x){
           r=mid-1;
         }else{
           l=mid+1;
         }
-      }
     }
-    cout<<l+1<<endl;
+    cout<<ans<<endl;
     
     return 0;
 }
- 	 	  		 		 		 	 		 	  	   	 	
