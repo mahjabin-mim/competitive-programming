5cc62182fc1888ba6cf2ac3a086132d98055554e
mahjabin-mim <mahjabinmim711@gmail.com>
2024-12-13 07:40:11 +0600

    added ordered set

---
+++ Topics/Data Structure/Ordered_Set.cpp
@@ -0,0 +1,41 @@
+/**
+ *    author:  Mahjabin7
+ *    created: 13.12.2024 04:19:38
+**/
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+ 
+int main() {    
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);    
+   
+   o_set<int> st;
+   st.insert(2);
+   st.insert(4);
+   st.insert(5);
+   st.insert(6);
+   st.insert(9);
+
+   for (auto x : st) {
+      cout << x << " ";
+   }
+   cout << '\n';
+
+   auto it = st.find_by_order(1);
+   cout << *it << '\n'; // access value by it's index
+
+   cout << st.order_of_key(5) << '\n'; // return number of elements < 5
+
+   return 0;
+}
